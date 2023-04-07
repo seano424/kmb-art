71023424d9db670da3f839b7348d147bb9c71854
Sean OReilly <soreilly424@gmail.com>
2023-04-07 16:03:55 -0500

    fixed images object

--- sanity/schemas/images-schema.ts
+++ sanity/schemas/images-schema.ts
@@ -1,22 +1,22 @@
 // The schema of the Milestone object-type
 const images = {
   name: 'images',
-  title: 'Images',
+  title: 'images',
   type: 'object',
   fields: [
     {
       name: 'image',
       title: 'Image',
       type: 'image',
-      description: 'Optional',
       options: { hotspot: true },
-      fields: [
-        {
-          name: 'alt',
-          title: 'Alt',
-          type: 'string',
-        },
-      ],
+    },
+    {
+      name: 'alt',
+      type: 'string',
+      title: 'Alt',
+      options: {
+        isHighlighted: true, // <-- make this field easily accessible
+      },
     },
   ],
 }
