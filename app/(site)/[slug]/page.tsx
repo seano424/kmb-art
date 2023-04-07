2dfd1da169daf5d006f97bc30e9176f6d92fa52c
Sean OReilly <soreilly424@gmail.com>
2023-04-07 11:05:19 -0500

    updating content

--- app/(site)/[slug]/page.tsx
+++ app/(site)/[slug]/page.tsx
@@ -14,16 +14,16 @@ export default async function Page({ params }: Props) {
       <h1 className='bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-5xl drop-shadow font-extrabold'>
         {page?.title}
       </h1>
-      <div className='relative h-[500px] w-full'>
-        {page.image && (
+      {page.image && (
+        <div className='relative h-[500px] w-full'>
           <Image
             src={page.image}
             alt={page.alt!}
             className='object-cover rounded-lg'
             fill
           />
-        )}
-      </div>
+        </div>
+      )}
       <div className='text-lg text-gray-700'>
         <PortableText value={page?.content} />
       </div>
