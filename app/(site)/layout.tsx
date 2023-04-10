483020030f7fd77b57c2f26644a7ae8450b480bc
Sean OReilly <soreilly424@gmail.com>
2023-04-10 13:45:38 -0500

    udpating headder

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,7 +1,7 @@
 import '../globals.css'
 import Link from 'next/link'
 import { getPages } from '@/sanity/sanity-utils'
-import { Roboto } from 'next/font/google'
+import { Roboto, Playfair_Display } from 'next/font/google'
 import clsx from 'clsx'
 
 const roboto = Roboto({
@@ -9,6 +9,8 @@ const roboto = Roboto({
   style: ['normal', 'italic'],
   subsets: ['latin'],
 })
+
+const playfair = Playfair_Display({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Karrie Marie Baxley, artist, painter',
@@ -25,19 +27,33 @@ export default async function RootLayout({
   return (
     <html lang='en'>
       <body className={clsx('max-w-6xl mx-auto py-10', roboto.className)}>
-        <header className='flex items-center justify-between'>
-          <Link
-            href='/'
-            className='bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-lg font-bold'
-          >
-            Work
+        <header className='flex flex-col items-center gap-5'>
+          <Link href='/'>
+            <h1
+              className={clsx(
+                'text-8xl font-bold text-[#211F20]',
+                playfair.className
+              )}
+            >
+              {' '}
+              <span className='bg-gradient-to-r from-cyan-400 via-sky-600 to-purple-800 bg-clip-text text-transparent'>
+                {' '}
+                Karrie Marie
+              </span>
+            </h1>
           </Link>
-          <div className='flex items-center gap-5 text-sm text-gray-600'>
+          <div className='flex items-center justify-center text-[#211F20] gap-10 text-xl'>
+            <Link
+              href='/'
+              className='hover:underline transition-all duration-300 ease-linear underline-offset-8'
+            >
+              Work
+            </Link>
             {pages.map((page) => (
               <Link
                 key={page._id}
                 href={`/${page.slug}`}
-                className='hover:underline'
+                className='hover:underline transition-all duration-300 ease-linear underline-offset-8'
               >
                 {page.title}
               </Link>
@@ -48,4 +64,14 @@ export default async function RootLayout({
       </body>
     </html>
   )
+}
+
+{
+  /* <h1 className='text-7xl font-extrabold text-gray-700'>
+        Hello I&apos;m
+        <span className='bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent'>
+          {' '}
+          Karrie Marie!
+        </span>
+      </h1> */
 }
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,43 +8,27 @@ export default async function Home() {
   console.log('art: ', artSeries)
 
   return (
-    <div>
-      <h1 className='text-7xl font-extrabold text-gray-700'>
-        Hello I&apos;m
-        <span className='bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent'>
-          {' '}
-          Karrie Marie!
-        </span>
-      </h1>
-      <p className='mt-3 text-xl text-gray-600'>
-        Hey everyone! Check out my art!
-      </p>
-      <h2 className='mt-24 font-bold text-gray-700 text-3xl'>
-        My Art Collection
-      </h2>
-
-      <div className='mt-5 grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
-        {artSeries.map((series) => (
-          <Link
-            href={`/projects/${series.slug}`}
-            key={series._id}
-            className='rounded-lg p-1 hover:scale-105 hover:border-blue-500 transition'
-          >
-            {series.image && (
-              <Image
-                src={series.image}
-                alt={series.alt ?? `Feature image for ${series.title}`}
-                width={750}
-                height={300}
-                className='object-cover rounded-lg'
-              />
-            )}
-            <div className='mt-2 font-extrabold bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent'>
-              {series.title}
-            </div>
-          </Link>
-        ))}
-      </div>
+    <div className='mt-5 grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
+      {artSeries.map((series) => (
+        <Link
+          href={`/projects/${series.slug}`}
+          key={series._id}
+          className='rounded-lg p-1 hover:scale-105 hover:border-blue-500 transition'
+        >
+          {series.image && (
+            <Image
+              src={series.image}
+              alt={series.alt ?? `Feature image for ${series.title}`}
+              width={750}
+              height={300}
+              className='object-cover rounded-lg'
+            />
+          )}
+          <div className='mt-2 font-extrabold bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent'>
+            {series.title}
+          </div>
+        </Link>
+      ))}
     </div>
   )
 }
