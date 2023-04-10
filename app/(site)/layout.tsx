d195479e8c4fba7368774971c74e3bd2a456a552
Sean OReilly <soreilly424@gmail.com>
2023-04-10 13:16:24 -0500

    adding clsx

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,9 +1,17 @@
 import '../globals.css'
-import Link from "next/link"
-import { getPages } from '@/sanity/sanity-utils';
+import Link from 'next/link'
+import { getPages } from '@/sanity/sanity-utils'
+import { Roboto } from 'next/font/google'
+import clsx from 'clsx'
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+})
 
 export const metadata = {
-  title: 'Karrie Marie Art',
+  title: 'Karrie Marie Baxley, artist, painter',
   description: 'Collection of artwork by Karrie Marie Baxley',
 }
 
@@ -12,22 +20,31 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-  const pages = await getPages();
+  const pages = await getPages()
 
   return (
-    <html lang="en">
-      <body className="max-w-4xl mx-auto py-10">
-        <header className="flex items-center justify-between">
-          <Link href="/" className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-lg font-bold">Karrie Marie Art</Link>
-          <div className="flex items-center gap-5 text-sm text-gray-600">
+    <html lang='en'>
+      <body className={clsx('max-w-6xl mx-auto py-10', roboto.className)}>
+        <header className='flex items-center justify-between'>
+          <Link
+            href='/'
+            className='bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent text-lg font-bold'
+          >
+            Work
+          </Link>
+          <div className='flex items-center gap-5 text-sm text-gray-600'>
             {pages.map((page) => (
-              <Link key={page._id} href={`/${page.slug}`} className="hover:underline">{page.title}</Link>
+              <Link
+                key={page._id}
+                href={`/${page.slug}`}
+                className='hover:underline'
+              >
+                {page.title}
+              </Link>
             ))}
-
           </div>
-
         </header>
-        <main className="py-20">{children}</main>
+        <main className='py-20'>{children}</main>
       </body>
     </html>
   )
