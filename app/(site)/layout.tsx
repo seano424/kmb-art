5e6053998f41aed92a7ca54ad855dec49be7fe15
Sean OReilly <soreilly424@gmail.com>
2023-04-11 10:06:37 -0500

    updated navigation

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,11 +1,11 @@
 import '../globals.css'
 import Link from 'next/link'
 import { getPages } from '@/sanity/sanity-utils'
-import { Montserrat, Poppins } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 import clsx from 'clsx'
+import Navigation from '../components/Navigation'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
-const poppins = Poppins({ weight: ['400', '700'], subsets: ['latin'] })
 
 export const metadata = {
   title: 'Karrie Marie Baxley, artist, painter',
@@ -22,40 +22,14 @@ export default async function RootLayout({
   return (
     <html lang='en'>
       <body className={clsx('max-w-8xl mx-auto py-20', montserrat.className)}>
-        <header className='flex flex-col gap-5 items-center uppercase'>
-          <Link className='text-2xl font-bold' href='/'>
+        <header className='flex flex-col gap-2 items-center uppercase'>
+          <Link className='text-2xl font-bold tracking-widest' href='/'>
             Karrie Marie
           </Link>
-          <div className='flex items-center justify-center gap-10'>
-            <Link
-              href='/work'
-              className='hover:underline transition-all duration-300 ease-linear underline-offset-8'
-            >
-              Work
-            </Link>
-            {pages.map((page) => (
-              <Link
-                key={page._id}
-                href={`/${page.slug}`}
-                className='hover:underline transition-all duration-300 ease-linear underline-offset-8'
-              >
-                {page.title}
-              </Link>
-            ))}
-          </div>
+          <Navigation pages={pages} />
         </header>
         <main className='py-10'>{children}</main>
       </body>
     </html>
   )
-}
-
-{
-  /* <h1 className='text-7xl font-extrabold text-gray-700'>
-        Hello I&apos;m
-        <span className='bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent'>
-          {' '}
-          Karrie Marie!
-        </span>
-      </h1> */
 }
---
+++ app/components/Navigation.tsx
@@ -0,0 +1,46 @@
+'use client'
+import Link from 'next/link'
+import clsx from 'clsx'
+import { usePathname } from 'next/navigation'
+
+interface Props {
+  pages: {
+    _id: string
+    slug: string
+    title: string
+  }[]
+}
+
+const Navigation = ({ pages }: Props) => {
+  const pathname = usePathname()
+
+  return (
+    <div className='flex items-center justify-center gap-10'>
+      <Link
+        href='/work'
+        className={clsx(
+          'hover:underline hover:text-gray-900 transition-all duration-150 ease-linear underline-offset-8',
+          pathname === '/work' ? 'text-gray-900' : 'text-gray-400'
+        )}
+      >
+        Work
+      </Link>
+      {pages.map((page) => (
+        <Link
+          key={page._id}
+          href={`/${page.slug}`}
+          className={clsx(
+            'hover:underline hover:text-gray-900 transition-all duration-150 ease-linear underline-offset-8',
+            pathname === `/${page.title.toLowerCase()}`
+              ? 'text-gray-900'
+              : 'text-gray-400'
+          )}
+        >
+          {page.title}
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+export default Navigation
