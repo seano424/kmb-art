989b35e61649249b56151e9930f682c96190babc
Sean OReilly <soreilly424@gmail.com>
2023-04-11 09:01:56 -0500

    clean up

--- app/(site)/work/page.tsx
+++ app/(site)/work/page.tsx
@@ -1,17 +1,8 @@
 import { Filter } from '@/app/components/Filter'
 import { getWorks } from '@/sanity/sanity-utils'
 
-const filters = [
-  { title: 'All Works', value: '' },
-  { title: 'Paintings', value: 'paintings' },
-  { title: 'Alcohol Inks', value: 'alcohol-inks' },
-  { title: 'Charcoals', value: 'charcoals' },
-]
-
 export default async function Works() {
   const works = await getWorks()
-
-  console.log('works: ', works)
 
   return (
     <div className='container'>
