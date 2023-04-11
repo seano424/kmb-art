201090cf49013aec04a07f845688900da3bc4e1a
Sean OReilly <soreilly424@gmail.com>
2023-04-11 17:25:42 -0500

    adding related events

--- app/(site)/work/[work]/page.tsx
+++ app/(site)/work/[work]/page.tsx
@@ -1,6 +1,7 @@
-import { getWork } from '@/sanity/sanity-utils'
+import { getWork, getWorks } from '@/sanity/sanity-utils'
 import { PortableText } from '@portabletext/react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 type Props = {
   params: { work: string }
@@ -9,13 +10,14 @@ type Props = {
 export default async function Work({ params }: Props) {
   const slug = params.work
   const work = await getWork(slug)
+  const works = await getWorks()
 
   return (
-    <div>
+    <div className='container border'>
       {work.description && (
         <p className='text-center pb-10'>{work.description}</p>
       )}
-      <div className='flex flex-wrap items-center justify-center gap-5'>
+      <div className='flex flex-wrap items-center justify-between gap-10'>
         {work.images &&
           work.images.map((img) => (
             <Image
@@ -27,6 +29,36 @@ export default async function Work({ params }: Props) {
               className='object-contain'
             />
           ))}
+      </div>
+
+      <div className='py-10'>
+        <p>Related Works</p>
+        <div className='grid grid-cols-3 gap-10 pt-5'>
+          {works
+            .filter((w) => w.title !== work.title)
+            .map((filteredWork) => (
+              <Link
+                href={`/work/${filteredWork.slug}`}
+                className='relative h-[500px] group'
+                key={filteredWork._id}
+              >
+                <Image
+                  src={filteredWork.featureImage}
+                  alt={
+                    filteredWork.featureImageAlt ??
+                    `Feature Image for ${filteredWork.title}`
+                  }
+                  className='rounded object-cover'
+                  fill
+                />
+                <div className='group-hover:bg-opacity-40 group-hover:opacity-100 opacity-0 absolute inset-0 flex justify-center items-center bg-opacity-0 bg-black transition-all duration-100 ease-linear'>
+                  <p className='uppercase text-3xl text-white'>
+                    {filteredWork.title}
+                  </p>
+                </div>
+              </Link>
+            ))}
+        </div>
       </div>
     </div>
   )
