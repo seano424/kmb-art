5f23775a2fd4c67b9e7dff1ed762fbf87c94e0a3
Sean OReilly <soreilly424@gmail.com>
2023-04-13 09:22:10 -0500

    remove console log

--- app/(site)/work/page.tsx
+++ app/(site)/work/page.tsx
@@ -3,8 +3,6 @@ import { getWorks } from '@/sanity/sanity-utils'
 
 export default async function Works() {
   const works = await getWorks()
-  console.log('works: ', works);
-  
 
   return (
     <div className='container'>
