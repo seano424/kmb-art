6e498a61bd9b09d6a92a8f949afc5a2cfb1f67fe
Sean OReilly <soreilly424@gmail.com>
2023-04-17 11:55:10 -0500

    adding head file

---
+++ app/head.tsx
@@ -0,0 +1,23 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        property='og:title'
+        content='Karrie Marie Baxley - Artist and Writer'
+      />
+      <meta property='og:image:url' content='https://nextjs.org/og-image.png' />
+      <meta property='og:image:width' content='800' />
+      <meta property='og:image:height' content='600' />
+      <meta name='robots' content='index' />
+      <meta
+        property='og:description'
+        content='Karrie Marie Baxley is an an artist based in Kansas City. She works in such mediums as paintings, charcoals, and inks.'
+      />
+      <meta
+        property='og:site_name'
+        content='Karrie Marie Baxley | Artist and Writer'
+      />
+      <link rel='icon' href='/images/favicon.ico' />
+    </>
+  )
+}
