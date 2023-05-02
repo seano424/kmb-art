4d9b749335f2edd9637dbcfcc6f37e05e523020d
Sean OReilly <soreilly424@gmail.com>
2023-05-02 14:28:14 -0500

    revalidating 3 pages

--- pages/api/revalidate-work.js
+++ pages/api/revalidate-work.js
@@ -8,6 +8,8 @@ export default async function handler(req, res) {
     // This should be the actual path not a rewritten path
     // e.g. for "/blog/[slug]" this should be "/blog/post-1"
     await res.revalidate(`/work/${res.body.slug}`)
+    await res.revalidate(`/work/`)
+    await res.revalidate(`/`)
     return res.json({ revalidated: true })
   } catch (err) {
     // If there was an error, Next.js will continue
