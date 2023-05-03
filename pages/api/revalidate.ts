353fced4fa17a7070d3a673116b0f7290613ad3c
Sean OReilly <soreilly424@gmail.com>
2023-05-03 09:00:59 -0500

    using valid signature

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -1,4 +1,4 @@
-import { isValidRequest } from '@sanity/webhook'
+import { SIGNATURE_HEADER_NAME, isValidSignature } from '@sanity/webhook'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 const secret = process.env.SANITY_WEBHOOK
@@ -6,10 +6,9 @@ const secret = process.env.SANITY_WEBHOOK
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   //authenticating the webhook
   try {
-    if (!isValidRequest(req, secret!)) {
-      res.status(401).json({ message: 'Invalid signature' })
-      return
-    }
+    const signature = req.headers[SIGNATURE_HEADER_NAME].toString()
+    if (!isValidSignature(JSON.stringify(req.body), signature, secret!))
+      return res.status(401).json({ msg: 'Invalid request!' })
 
     //getting payload
     const { slug } = req.body
@@ -17,9 +16,9 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     await res.revalidate(`/work/`)
     await res.revalidate(`/work/${slug}`)
 
-    res.status(200).json({ msg: 'Art pages revalidated.' })
+    res.status(200).json({ msg: 'Product pages revalidated.' })
   } catch (error) {
-    res.status(500).json({ err: 'Something went wrong!' })
+    res.status(500).json({ err: 'Something went Wrong!' })
   }
 }
 
