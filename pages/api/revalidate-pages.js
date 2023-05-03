6d0f970e319cbc51a3e891da7240b745d178333e
Sean OReilly <soreilly424@gmail.com>
2023-05-03 09:17:43 -0500

    two api routes

---
+++ pages/api/revalidate-pages.js
@@ -0,0 +1,22 @@
+import { SIGNATURE_HEADER_NAME, isValidSignature } from '@sanity/webhook'
+
+const secret = process.env.SANITY_WEBHOOK
+
+const handler = async (req, res) => {
+  //authenticating the webhook
+  try {
+    const signature = req.headers[SIGNATURE_HEADER_NAME].toString()
+    if (!isValidSignature(JSON.stringify(req.body), signature, secret))
+      return res.status(401).json({ msg: 'Invalid request!' })
+
+    //getting payload
+    const { slug } = req.body
+    await res.revalidate(`/${slug}`)
+
+    res.status(200).json({ msg: 'Single pages revalidated.' })
+  } catch (error) {
+    res.status(500).json({ err: 'Something went Wrong!' })
+  }
+}
+
+export default handler
