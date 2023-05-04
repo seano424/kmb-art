3503c8e8f81d0817d48cdae9e886730f482ea77c
Sean OReilly <soreilly424@gmail.com>
2023-05-04 15:07:19 -0500

    creating nav logo component

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,9 +1,7 @@
 import clsx from 'clsx'
 import { getNavItems } from '@/sanity/sanity-utils'
 import NavItemsDesktop from './NavItemsDesktop'
-import Link from 'next/link'
-import Image from 'next/image'
-import { urlFor } from '@/sanity/sanity-utils'
+import NavLogo from './NavLogo'
 
 export default async function Nav() {
   const navItems = await getNavItems()
@@ -16,26 +14,7 @@ export default async function Nav() {
       )}
     >
       <div className='px-10 w-full flex items-center justify-between gap-2'>
-        {navItems.logoImg ? (
-          <Link
-            href='/'
-            className='relative w-[300px] md:w-[200px] lg:w-[400px] h-24 lg:h-36'
-          >
-            <Image
-              src={urlFor(navItems.logoImg).url()}
-              alt='Logo for Karrie Marie Baxley'
-              className='object-cover rounded-sm'
-              fill
-            />
-            <div className='absolute inset-0 flex justify-center items-center font-black text-white text-center lg:text-3xl'>
-              <p>{navItems.logoTitle ?? ''}</p>
-            </div>
-          </Link>
-        ) : (
-          <Link href='/' className='text-3xl font-bold tracking-widest'>
-            {navItems.logoTitle ?? 'Karrie Marie'}
-          </Link>
-        )}
+        <NavLogo {...navItems} />
         <NavItemsDesktop {...navItems} />
       </div>
     </nav>
---
+++ app/components/NavLogo.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { NavItems } from '@/types/NavItems'
+import { urlFor } from '@/sanity/sanity-utils'
+
+export default function NavLogo(navItems: NavItems) {
+  return (
+    <>
+      {navItems.logoImg ? (
+        <Link
+          href='/'
+          className='relative w-[300px] md:w-[200px] lg:w-[400px] h-24 lg:h-36'
+        >
+          <Image
+            src={urlFor(navItems.logoImg).url()}
+            alt='Logo for Karrie Marie Baxley'
+            className='object-cover rounded-sm'
+            fill
+          />
+          <div className='absolute inset-0 flex justify-center items-center font-black text-white text-center lg:text-3xl'>
+            <p>{navItems.logoTitle ?? ''}</p>
+          </div>
+        </Link>
+      ) : (
+        <Link href='/' className='text-3xl font-bold tracking-widest'>
+          {navItems.logoTitle ?? 'Karrie Marie'}
+        </Link>
+      )}
+    </>
+  )
+}
