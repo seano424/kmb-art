d7f0ac271e8132252dc9c30f9cb047a0555ccc4d
Sean OReilly <soreilly424@gmail.com>
2023-05-11 10:09:00 -0500

    remove edge func

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,12 +1,10 @@
 import { gethomepageImages } from '@/sanity/sanity-utils'
 import ImageGrid from '../components/ImageGrid'
 
-export const revalidate = 604800
-
 export default async function Home() {
   const images = await gethomepageImages()
-  console.log('this is a test');
-  
+  console.log('this is a test')
+
   return (
     <div className='py-10 md:py-5'>
       {images && <ImageGrid images={images} size='lg' />}
