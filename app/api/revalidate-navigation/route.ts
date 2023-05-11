4a578947c760cacfcb01e2238623e8f8319ff1a3
Sean OReilly <soreilly424@gmail.com>
2023-05-11 09:19:08 -0500

    updating to get request

--- app/api/revalidate-navigation/route.ts
+++ app/api/revalidate-navigation/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { revalidateTag } from 'next/cache'
 
-export async function POST(request: NextRequest) {
+export async function GET(request: NextRequest) {
   const tag = await request.headers.get('tag')
 
   if (tag !== 'navigation') {
