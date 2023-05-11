46b1e0629f32c701c27d4e179a9c1ba26f8c8bfd
Sean OReilly <soreilly424@gmail.com>
2023-05-11 16:36:48 -0500

    test commit

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -3,6 +3,7 @@ import ImageGrid from '../components/ImageGrid'
 
 export default async function Home() {
   const images = await getHomepageImages()
+  console.log('hello world')
 
   return (
     <div className='py-10 md:py-5'>
