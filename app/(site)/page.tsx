017da64f5069d952b89db1a957a36449c671d4fa
Sean OReilly <soreilly424@gmail.com>
2023-05-11 09:47:29 -0500

    commit comment

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -5,7 +5,8 @@ export const revalidate = 604800
 
 export default async function Home() {
   const images = await gethomepageImages()
-
+  console.log('this is a test');
+  
   return (
     <div className='py-10 md:py-5'>
       {images && <ImageGrid images={images} size='lg' />}
