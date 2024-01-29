574af35d5e948379924f04e7dd96bc05d1e24695
seano424 <soreilly424@gmail.com>
2024-01-29 12:50:23 -0600

    adding cta to more art

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import urlFor from '@/sanity/urlFor'
 import getFeaturedImage from '@/sanity/hooks/getFeaturedImage'
 
@@ -6,16 +7,27 @@ export default async function Home() {
   const featuredImage = await getFeaturedImage()
 
   return (
-    <div className="py-10 md:py-5">
+    <>
       {/* Hero */}
-      <div className="bg-purple-500 w-full h-[500px] lg:h-[600px]">
+      <section className="bg-purple-500 w-full h-[500px] lg:h-[600px] relative">
         <Image
           className="object-cover"
           src={urlFor(featuredImage.featureImage.asset._ref).height(800).url()}
           alt={featuredImage.featureImage.alt}
           fill
         />
-      </div>
-    </div>
+      </section>
+
+      {/* CTA - All Art */}
+      <section className="flex flex-col gap-5 items-center my-20">
+        <h2 className="text-3xl max-w-md text-center tracking-wide leading-relaxed">Explore more of the artwork by Karrie Marie Baxley</h2>
+        <Link
+          className="bg-gray-600 text-white px-16 py-4 uppercase text-lg"
+          href={'/work'}
+        >
+          See more art
+        </Link>
+      </section>
+    </>
   )
 }
