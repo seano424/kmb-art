fe8651ff90a6f1ecaa357cdad9babcad7cdc7378
seano424 <soreilly424@gmail.com>
2024-01-29 13:02:54 -0600

    adding two sections to home

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,9 +2,11 @@ import Image from 'next/image'
 import Link from 'next/link'
 import urlFor from '@/sanity/urlFor'
 import getFeaturedImage from '@/sanity/hooks/getFeaturedImage'
+import getWorks from '@/sanity/hooks/getWorks'
 
 export default async function Home() {
   const featuredImage = await getFeaturedImage()
+  const works = await getWorks()
 
   return (
     <>
@@ -20,13 +22,37 @@ export default async function Home() {
 
       {/* CTA - All Art */}
       <section className="flex flex-col gap-5 items-center my-20">
-        <h2 className="text-3xl max-w-md text-center tracking-wide leading-relaxed">Explore more of the artwork by Karrie Marie Baxley</h2>
+        <h2 className="text-3xl max-w-md text-center tracking-wide leading-relaxed">
+          Explore more of the artwork by Karrie Marie Baxley
+        </h2>
         <Link
           className="bg-gray-600 text-white px-16 py-4 uppercase text-lg"
           href={'/work'}
         >
           See more art
         </Link>
+      </section>
+
+      {/* Featured Series */}
+      <section className="grid lg:grid-cols-2 gap-5 my-20">
+        {works &&
+          works.slice(0, 2).map((work) => (
+            <Link
+              href={`/work/${work.slug}`}
+              className="relative h-[700px]"
+            >
+              <div className="absolute inset-0 p-10 z-40 flex flex-col justify-between text-gray-50">
+                <h3 className="text-3xl">{work.title}</h3>
+                <p className="text-xl underline underline-offset-4">See more</p>
+              </div>
+              <Image
+                className="object-cover"
+                src={urlFor(work.featureImage).height(800).url()}
+                alt={work.featureImage.alt}
+                fill
+              />
+            </Link>
+          ))}
       </section>
     </>
   )
